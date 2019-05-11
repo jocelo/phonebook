017e1fb075d5c77c97eddaa5d55aeefba067113b
Alfredo Alonso <jocelo@Alfredos-MBP.fios-router.home>
2019-05-11 00:43:36 -0400

    more libraries added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Phonebook</h1>
+      <hr />
+      <button>Add new contact</button>
+      <div></div>
+      <hr />
+      <div>
+        <p>Emergency Contact: Alfredo Alonso (847) 123 1234</p>
+      </div>
+      <hr />
+      <div>
+        <h3>Contacts</h3>
+        <div>
+          <h4>_____ A _____</h4>
+          <div>
+          <p> <strong>Alfredo Alonso</strong> (847) 123 1234 </p>
+          <p> <strong>Arturo Mendoza</strong> (310) 321 8859 </p>
+          </div>
+        </div>
+
+        <div>
+          <h4>_____ B _____</h4>
+          <div>
+          <p> <strong>Benito Juarez</strong> (449) 345 9898 </p>
+          </div>
+        </div>
+
+        <div>
+          <h4>_____ F _____</h4>
+          <div>
+          <p> <strong>Frida Kahlo</strong> (555) 553 5351 </p>
+          </div>
+        </div>
+
+      </div>
     </div>
   );
 }
