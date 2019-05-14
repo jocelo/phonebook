94ec2dfb332642aa3860ddada1e6c75c504b64c0
alfredo.alonso <alfredo.alonso@prodapt.com>
2019-05-14 16:50:51 -0400

    tabbing format fixes

--- src/components/home.js
+++ src/components/home.js
@@ -8,75 +8,75 @@ import { connect } from 'react-redux';
 import { fetchContacts } from '../actions/contacts';
 
 class Home extends React.Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			emergency: {
-				id: 1,
-				name: 'Alfredo Alonso',
-				phone: '(847) 123 1234'
-			}
-		};
+    this.state = {
+      emergency: {
+        id: 1,
+        name: 'Alfredo Alonso',
+        phone: '(847) 123 1234'
+      }
+    };
 
-		this.handleSaveContact = this.handleSaveContact.bind(this);
-		this.setEmergencyContact = this.setEmergencyContact.bind(this);
-	}
+    this.handleSaveContact = this.handleSaveContact.bind(this);
+    this.setEmergencyContact = this.setEmergencyContact.bind(this);
+  }
 
-	componentWillMount() {
-		this.props.fetchContacts();
-	}
+  componentWillMount() {
+    this.props.fetchContacts();
+  }
 
-	handleSaveContact(contactName, contactPhone) {
-		const initialLetter = contactName.substr(0,1).toUpperCase(),
-			contacts = this.state.contacts;
-		let recordFound = contacts.filter(record=>record.letter === initialLetter);
-		
-		if (recordFound.length === 1) {
-			recordFound = recordFound[0];
-		} else {
-			const newLength = contacts.push({
-				letter: initialLetter,
-				list: []
-			})
-			recordFound = contacts[newLength-1];
-		}
+  handleSaveContact(contactName, contactPhone) {
+    const initialLetter = contactName.substr(0,1).toUpperCase(),
+      contacts = this.state.contacts;
+    let recordFound = contacts.filter(record=>record.letter === initialLetter);
+    
+    if (recordFound.length === 1) {
+      recordFound = recordFound[0];
+    } else {
+      const newLength = contacts.push({
+        letter: initialLetter,
+        list: []
+      })
+      recordFound = contacts[newLength-1];
+    }
 
-		recordFound.list.push({
-			id: recordFound.list.length + 1,
-			name: contactName,
-			phone: contactPhone
-		})
+    recordFound.list.push({
+      id: recordFound.list.length + 1,
+      name: contactName,
+      phone: contactPhone
+    })
 
-		// this.setState({contacts: contacts});
-	}
+    // this.setState({contacts: contacts});
+  }
 
-	setEmergencyContact(contactId, contactLetter) {
-		const firstLevel = this.state.contacts.filter(contactGroup=>contactGroup.letter === contactLetter);
-		const secondLevel = firstLevel[0].list.filter(contact=>contact.id === contactId);
-		this.setState({emergency: secondLevel[0]});
-	}
+  setEmergencyContact(contactId, contactLetter) {
+    const firstLevel = this.state.contacts.filter(contactGroup=>contactGroup.letter === contactLetter);
+    const secondLevel = firstLevel[0].list.filter(contact=>contact.id === contactId);
+    this.setState({emergency: secondLevel[0]});
+  }
 
-	render() {
-		return (
-			<div>
-				<h1>Phonebook</h1>
-				<hr />
-				<NewContact saveNewContact={this.handleSaveContact}></NewContact>
-				<hr />
-				<EmergencyContact contact={this.state.emergency}></EmergencyContact>
-				<hr />
-				<ContactList listOfContacts={this.state.contacts} updateEmergencyContactParent={this.setEmergencyContact}></ContactList>
-			</div>
-		)
-	}
+  render() {
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <hr />
+        <NewContact saveNewContact={this.handleSaveContact}></NewContact>
+        <hr />
+        <EmergencyContact contact={this.state.emergency}></EmergencyContact>
+        <hr />
+        <ContactList listOfContacts={this.state.contacts} updateEmergencyContactParent={this.setEmergencyContact}></ContactList>
+      </div>
+    )
+  }
 }
 
 const mapsStateToProps = state=>({
-	contacts: state.contacts
+  contacts: state.contacts
 })
 
 export default connect(
-	mapsStateToProps,
-	{ fetchContacts }
+  mapsStateToProps,
+  { fetchContacts }
 )(Home);
--- src/components/individual.js
+++ src/components/individual.js
@@ -1,26 +1,26 @@
 import React from 'react';
 
 class IndividualContact extends React.Component {
-	
-	constructor(props) {
-		super(props);
+  
+  constructor(props) {
+    super(props);
 
-		this.markAsEmergency = this.markAsEmergency.bind(this);
-	}
+    this.markAsEmergency = this.markAsEmergency.bind(this);
+  }
 
-	markAsEmergency() {
-		this.props.setEmergencyContact(this.props.id, this.props.letter);
-	}
+  markAsEmergency() {
+    this.props.setEmergencyContact(this.props.id, this.props.letter);
+  }
 
-	render() {
-		return (
-			<div>
-				<p> 
-					<strong>{this.props.name}</strong> {this.props.phone} <button onClick={this.markAsEmergency}>Set Emergency</button>
-				</p> 
-			</div>
-		)
-	}
+  render() {
+    return (
+      <div>
+        <p> 
+          <strong>{this.props.name}</strong> {this.props.phone} <button onClick={this.markAsEmergency}>Set Emergency</button>
+        </p> 
+      </div>
+    )
+  }
 }
 
 export default IndividualContact;
